ab6a6995c12df2b3f0ad3c4c032538af29bd3cb4
Danilo Lemos <162503728+danilo01arrudal@users.noreply.github.com>
2026-06-10 18:09:18 -0300

    Update variables.tf

--- AWS/Data Engineering/terraform/variables.tf
+++ AWS/Data Engineering/terraform/variables.tf
@@ -1,1 +1,17 @@
+variable "aws_region" {
+  description = "Região AWS onde os recursos serão criados"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "raw_bucket_name" {
+  description = "Nome do bucket S3 para dados brutos"
+  type        = string
+  default     = "raw-bucket"
+}
+
+variable "consume_bucket_name" {
+  description = "Nome do bucket S3 para dados processados"
+  type        = string
+  default     = "consume-bucket"
+}
